6792e10a69ee8658752ec9b4f03ddd0647c08159
Gunslingahhh <abgmhafizhan1908@gmail.com>
2024-12-05 23:49:16 +0800

    Fix bootstrap js import

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Field Booking System</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/index.css">
     </head>
     <body>
@@ -49,5 +49,6 @@
                 </div>
             </div>
         </nav>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
