d611bd9b91da9fd53f4a4e101ac47df40c10ebb2
Gunslingahhh <abgmhafizhan1908@gmail.com>
2024-12-05 23:16:23 +0800

    Prepare basic file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    // Include the database connection file
+    include "php/connection.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Field Booking System</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
